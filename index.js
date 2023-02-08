7e421b64c43d0b698473fe73488e1677838db84f
josuhinrichs <josue2001marinho@gmail.com>
2023-02-08 12:58:42 -0300

    É possível modificar o grid e limpar a tela

--- index.js
+++ index.js
@@ -3,39 +3,71 @@ const gridContainer = document.querySelector('#grid-container');
 const colorPicker = document.getElementById('picked-color');
 const colorModes = ['color-mode', 'eraser', 'crazy-mode', 'da-vinci'];
 let actualColorMode = colorModes[0];
+const rangeSlider = document.getElementById("range-slider");
+const rangeSliderSubtitle = document.getElementById("slider-subtitle");
+let actualGrid = 16;
+
+let pixelDivs = null;
+let outerDivs = null;
 
 let pickedColor = 'rgb(255,255,255)';
 let canvasSize = 16;
-let cellSize = 640/canvasSize - 0;
+let cellSize = 640/actualGrid - 0;
 let mousePressed = false;
 
 let mouseDown = false;
 
 let colorButtons = document.querySelectorAll('.color-button');
 
-for (let j=0; j<canvasSize; j++) {
-    const outerDiv = document.createElement('div');
-    outerDiv.classList.add('outer-div');
-    gridContainer.appendChild(outerDiv);
-    
-    for(let i=0; i<canvasSize; i++){
-        const div = document.createElement('div');
-        div.classList.add('child');
-        outerDiv.appendChild(div);
-    }
-}
-
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
-colorPicker.addEventListener('change', updateColorPicker);
+colorPicker.addEventListener('change', (e) => {
+    updateColorPicker(e);
+});
 
 function updateColorPicker(e) {
     pickedColor = e.target.value;
+    console.log(pickedColor);
+}
+
+rangeSlider.addEventListener('change', (e) => {
+    updateGrid(e.target.value)
+});
+
+function updateGrid(value) {
+    actualGrid = value;
+    rangeSliderSubtitle.textContent = `${actualGrid}x${actualGrid}`;
+    restartGrid();
+}
+
+function restartGrid(){
+    if(outerDivs != null){
+        outerDivs.forEach(item => {
+            item.remove()
+        })
+    }
+    
+    for (let j=0; j<actualGrid; j++) {
+        const outerDiv = document.createElement('div');
+        outerDiv.classList.add('outer-div');
+        gridContainer.appendChild(outerDiv);
+        
+        for(let i=0; i<actualGrid; i++){
+            const div = document.createElement('div');
+            div.classList.add('child');
+            outerDiv.appendChild(div);
+        }
+    }
+
+    pixelDivs = document.querySelectorAll('.child');
+    outerDivs = document.querySelectorAll('.outer-div');
+    cellSize = (640/actualGrid - 0) + 'px';
+    addListeners();
 }
 
 function changeColorMode(e){
-    mouseToggle();
+    //mouseToggle();
     colorButtons.forEach(item => {
         item.classList.remove('active-button');
     });
@@ -59,26 +91,15 @@ function changeColorMode(e){
     }
 }
 
-function getRandomRGB() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    return "rgb(" + x + "," + y + "," + z + ")";  
-}
-
-function darkenRGB() {
-
-}
-
 colorButtons.forEach(item => {
     item.addEventListener('click', (e)=>{
         changeColorMode(e);
-        console.log(actualColorMode);
     })
 });
 
 function changeColor(e) {
     if(mousePressed == false) return;
+    console.log("testing");
 
     switch(actualColorMode){
         case colorModes[0]:{
@@ -104,24 +125,39 @@ function mouseToggle() {
     if(mousePressed == true) { mousePressed = false } else mousePressed = true;
 }
 
-const pixelDivs = document.querySelectorAll('.child');
+function addListeners(){
+    pixelDivs.forEach( item => {
+        item.addEventListener('click', (e) => {
+            mouseToggle();
+            changeColor(e);
+        });
+    
+        item.addEventListener('mouseover', e => changeColor(e));
 
-
-pixelDivs.forEach( item => {
-    item.addEventListener('click', (e) => {
-        mouseToggle();
-        changeColor(e);
+        item.style.width = cellSize;
+        item.style.height = cellSize;
     });
-
-    item.addEventListener('mouseover', e => changeColor(e));
-});
+}
 
 function clearCanvas(){
-    console.log("eeee");
+    
     pixelDivs.forEach(item => {
-        item.target.style.backgroundColor = '#FFFFFF';
+        item.style.backgroundColor = '#FFFFFF';
     });
 }
 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', clearCanvas);
+
+function getRandomRGB() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    return "rgb(" + x + "," + y + "," + z + ")";  
+}
+
+function darkenRGB() {
+
+}
+
+updateGrid(16);
